e5e50f8db8de906ed889f4a2db3cccecc5a6b847
amalirajapaksha <amali2022ugc@gmail.com>
2026-06-28 17:53:54 +0530

    updated

---
+++ R/GNpop_by_EthnicGroup.R
@@ -0,0 +1,13 @@
+#' Population by Ethnic Group According to Grama Niladhari Division
+#'
+#' Distribution of population by ethnic group across Grama Niladhari Divisions (GND) in Sri Lanka
+#' based on the 2024 Census of Population and Housing.
+#'
+#'
+#'
+#' @format A data frame.
+#'
+#' @source Department of Census and Statistics, Sri Lanka.
+#' Census of Population and Housing 2024.
+#'
+"GNpop_by_EthnicGroup"
