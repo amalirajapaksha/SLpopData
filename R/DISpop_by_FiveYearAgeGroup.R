cfefb95b26c24783073b2b71e47d51a1a6aa914e
amalirajapaksha <amali2022ugc@gmail.com>
2026-08-06 13:18:15 +0530

    updated

---
+++ R/DISpop_by_FiveYearAgeGroup.R
@@ -0,0 +1,13 @@
+#' Population by Five-Year Age Groups According to District
+#'
+#' Distribution of the population by five-year age groups at the district level
+#' in Sri Lanka based on the 2012 and 2024 Censuses of Population and Housing.
+#'
+#' @format A data frame.
+#'
+#' @source Department of Census and Statistics, Sri Lanka.
+#' Censuses of Population and Housing 2012 and 2024.
+#'
+#' @examples
+#' head(DISpop_by_FiveYearAgeGroup)
+"DISpop_by_FiveYearAgeGroup"
